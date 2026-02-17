fcefecc2a5fc7b6c6bd312564c5d979fc3e6a454
princekushwaha9142 <princekushwa9142@gmail.com>
2026-02-18 00:01:34 +0530

    SET OPERATIONS

---
+++ Python Lesson 12 - Sets (set)/SET OPERATIONS.py
@@ -0,0 +1,30 @@
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+# Union (Combine)
+print(a | b)
+
+# Output: {1,2,3,4,5,6}
+
+# Intersection (Common)
+
+print(a & b)
+
+# Output: {3,4}
+
+# Difference
+
+print(a - b)
+
+# Output: {1,2}
+
+# Symmetric Difference 
+print(a ^ b)
+
+# Output: {1,2,5,6}
+
+nums = {10, 20, 30}
+
+print(20 in nums)   
+
+# Used in performance-heavy code.
