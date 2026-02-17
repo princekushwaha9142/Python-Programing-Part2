374d964214b96f4525c925bc3e79bf43563a3357
princekushwaha9142 <princekushwa9142@gmail.com>
2026-02-17 23:54:59 +0530

    Set Methods

---
+++ Python Lesson 12 - Sets (set)/Set Methods.py
@@ -0,0 +1,17 @@
+# add()
+
+nums = {10, 20}
+nums.add(30)
+
+# remove()
+nums.remove(20)
+
+# Error if not found.
+
+# Safer:
+nums.discard(99)   # No error 
+
+# pop() 
+nums.pop()
+
+# Removes random element 
