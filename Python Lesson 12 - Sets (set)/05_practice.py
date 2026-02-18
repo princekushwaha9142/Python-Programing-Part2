e59dd46e6bd551077164f9861d934421144615a1
princekushwaha9142 <princekushwa9142@gmail.com>
2026-02-18 22:41:52 +0530

    Union & intersection

---
+++ Python Lesson 12 - Sets (set)/05_practice.py
@@ -0,0 +1,8 @@
+# Find union & intersection
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+union = set1 | set2
+intersection = set1 & set2
+print("Union:", union)
+print("Intersection:", intersection)
