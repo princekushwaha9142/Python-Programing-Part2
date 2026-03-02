ddfb2af1a3c520c8a294f950c86bc71f32fe8b91
princekushwaha9142 <princekushwa9142@gmail.com>
2026-03-02 15:21:55 +0530

    FUnction

---
+++ Python Lesson 14 - Functions/Function.py
@@ -0,0 +1,13 @@
+# a function is a relationship between inputs where each input is related to exactly one output
+
+# Instead of repeating logic ❌
+print("Hello")
+print("Hello")
+print("Hello")
+
+# we write a function ✅
+def greet():
+    print("Hello")
+
+greet()
+greet()
