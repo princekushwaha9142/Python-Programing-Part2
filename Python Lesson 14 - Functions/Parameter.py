a9b3120378f0e7b1837db62aaff9e857495af47b
princekushwaha9142 <princekushwa9142@gmail.com>
2026-03-02 15:35:36 +0530

    Parameter

---
+++ Python Lesson 14 - Functions/Parameter.py
@@ -0,0 +1,18 @@
+# Parameters = Input to function.
+
+def greet(name):
+    print("Hello", name)
+
+greet("Prince")
+greet("Alex")
+
+# name → parameter
+# "Prince" → argument
+
+# Multiple Parameters
+
+def add(a, b):
+    print(a + b)
+
+add(10, 20)
+
