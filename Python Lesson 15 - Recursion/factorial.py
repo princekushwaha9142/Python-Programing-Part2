d702f27b2acaef45d50b019af7423ec9501751e2
princekushwaha9142 <princekushwa9142@gmail.com>
2026-03-03 22:10:30 +0530

    factorial

---
+++ Python Lesson 15 - Recursion/factorial.py
@@ -0,0 +1,10 @@
+# Factorial formula:
+
+# n! = n × (n-1) × (n-2) × ... × 1
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+n = int(input("Enter a number: "))
+print(factorial(n))
