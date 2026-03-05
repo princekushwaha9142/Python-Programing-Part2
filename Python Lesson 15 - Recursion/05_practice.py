3ae0f8099200c1676af7514bbed9a53847a58481
princekushwaha9142 <princekushwa9142@gmail.com>
2026-03-05 21:24:36 +0530

    Check palindrome

---
+++ Python Lesson 15 - Recursion/05_practice.py
@@ -0,0 +1,13 @@
+# Check palindrome
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
+string = input("Enter a string: ")
+if is_palindrome(string):
+    print(string, "is a palindrome.")
+else:
+    print(string, "is not a palindrome.")
+    
