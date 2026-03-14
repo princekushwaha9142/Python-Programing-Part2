47bb56ab2f4e061e9aae478813e5b0f31bbeaf5c
princekushwaha9142 <princekushwa9142@gmail.com>
2026-03-14 17:22:10 +0530

    init

---
+++ Python Lesson 17 - Object-Oriented Programming (OOP)/init.py
@@ -0,0 +1,21 @@
+class student:
+     def __init__(self, name, age):
+          self.name = name
+          self.age = age
+
+s1 = student("Prince", 20)
+print(s1.name, s1.age)
+
+
+class Laptop:
+
+    def __init__(self, brand, ram):
+        self.brand = brand
+        self.ram = ram
+
+l1 = Laptop("HP", "16GB")
+print(l1.brand, l1.ram)
+          
+
+
+          
