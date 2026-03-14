faa797aa680b7792f2467d95688cbf1d479d280f
princekushwaha9142 <princekushwa9142@gmail.com>
2026-03-14 17:12:39 +0530

    class

---
+++ Python Lesson 17 - Object-Oriented Programming (OOP)/class.py
@@ -0,0 +1,9 @@
+class Employee:
+    language = "Python"
+    salary = 500000
+
+prince = Employee()
+print(prince.language, prince.salary)
+
+rohan = Employee()
+print(rohan.language, rohan.salary)
